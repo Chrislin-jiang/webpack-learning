7e944498d6fc203b66cf2d4b919ee1a9f780d97a
gongjianglin6 <gongjlg@yonyou.com>
2022-09-12 20:07:48 +0800

    feat: add examples

---
+++ src/examples/example2/build.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const path = require('path')
+
+function build1() {
+  return webpack({
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'build')
+    },
+    mode: 'none'
+  })
+}
+
+build1()
+  .run((err, stat) => {
+    const startTime = stat.startTime
+    const endTime = stat.endTime
+    console.log("构建时间", endTime - startTime);
+    // console.log("构建时间", stat.endTime - stat.startTime);
+  })
+
+// build1()
+//   .run((err, stats) => {
+//     const time = stats.toJson().time;
+//     console.log("构建时间", time);
+//   })
