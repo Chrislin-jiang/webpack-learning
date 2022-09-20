09990e735183901326a030271f192e2d76300204
gongjianglin6 <gongjlg@yonyou.com>
2022-09-20 11:12:06 +0800

    feat: analyze main.js

--- src/examples/example2/build/main-analyze.js
+++ src/examples/example2/build/main-analyze.js
@@ -17,22 +17,23 @@
   /******/ 	
   /******/ 	// The require function
   /******/ 	function __webpack_require__(moduleId) {
-  /******/ 		// Check if module is in cache
+  /******/ 		// （1）检查模块是否存在于缓存中
   /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+              // （2）如果缓存存在，则直接返回缓存中 moduleId 所对应的 exports 对象
   /******/ 		if (cachedModule !== undefined) {
   /******/ 			return cachedModule.exports;
   /******/ 		}
-  /******/ 		// Create a new module (and put it into the cache)
+  /******/ 		// （3）如果缓存不存在，则创建一个 module 对象，用于初始化 module 信息，同时创建对应的缓存
   /******/ 		var module = __webpack_module_cache__[moduleId] = {
   /******/ 			// no module.id needed
   /******/ 			// no module.loaded needed
   /******/ 			exports: {}
   /******/ 		};
   /******/ 	
-  /******/ 		// Execute the module function
+  /******/ 		// （4）通过 moduleId 匹配到 __webpack_modules__ 存放的对应的模块，并传入 module, module.exports, __webpack_require__ 参数，执行模块
   /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
   /******/ 	
-  /******/ 		// Return the exports of the module
+  /******/ 		// （5）返回 module.exports
   /******/ 		return module.exports;
   /******/ 	}
   /******/ 	
