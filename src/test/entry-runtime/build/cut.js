a90cde30859104db5d35c07feb60ef54f0e15a0e
gongjianglin6 <gongjlg@yonyou.com>
2022-11-09 08:43:16 +0800

    feat: add entry~runtime

---
+++ src/test/entry-runtime/build/cut.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([[0],{
+
+/***/ 2:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const cut = (a, b) => {
+  return a - b;
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (cut);
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(2));
+/******/ }
+]);
