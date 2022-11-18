5b25a40236ab7c9cf8b428002a82cc05e490f2d3
gongjianglin6 <gongjlg@yonyou.com>
2022-11-18 11:11:43 +0800

    feat: update

--- src/examples/example2/build.js
+++ src/examples/example2/build.js
@@ -1,32 +1,32 @@
 const webpack = require('webpack')
 const path = require('path')
 
-// const compiler = webpack({
-//   entry: './index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'build')
-//   },
-//   mode: 'none'
-// })
-
-// compiler.run((err, stats) => {
-//   const startTime = stats.startTime
-//   const endTime = stats.endTime
-//   console.log("构建时间", endTime - startTime);
-// })
-
-webpack({
-  // [配置对象](/configuration/)
+const compiler = webpack({
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'build')
   },
   mode: 'none'
-}, (err, stats) => { // [Stats Object](#stats-object)
-  if (err || stats.hasErrors()) {
-    // [在这里处理错误](#error-handling)
-  }
+})
+
+compiler.run((err, stats) => {
   const startTime = stats.startTime
   const endTime = stats.endTime
   console.log("构建时间", endTime - startTime);
-});
+})
+
+// webpack({
+//   // [配置对象](/configuration/)
+//   entry: './index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'build')
+//   },
+//   mode: 'none'
+// }, (err, stats) => { // [Stats Object](#stats-object)
+//   if (err || stats.hasErrors()) {
+//     // [在这里处理错误](#error-handling)
+//   }
+//   const startTime = stats.startTime
+//   const endTime = stats.endTime
+//   console.log("构建时间", endTime - startTime);
+// });
