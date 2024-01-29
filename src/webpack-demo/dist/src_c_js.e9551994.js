2c5a6bb56a01717b53a1353dfcd5adc03aa42250
gongjianglin <gongjlg@yonyou.com>
2024-01-29 09:37:30 +0800

    feat: update

---
+++ src/webpack-demo/dist/src_c_js.e9551994.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || []).push([["src_c_js"],{
+
+/***/ "./src/c.js":
+/*!******************!*\
+  !*** ./src/c.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ sub)\n/* harmony export */ });\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'core-js/modules/es.promise.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'core-js/modules/web.dom-collections.iterator.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n/* harmony import */ var _d__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/d */ \"./src/d.js\");\n\n\n\nPromise.resolve(/*! import() */).then(__webpack_require__.bind(__webpack_require__, /*! @/b.js */ \"./src/b.js\")).then(b => b.add(200, 100));\nfunction sub(n1, n2) {\n  return n1 - n2 + (0,_d__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(100, 50);\n}\n\n//# sourceURL=webpack://webpack-demo/./src/c.js?");
+
+/***/ })
+
+}]);
